3aede072f4ef2e6ae5d19fdc6103710228ac5ffc
LucasDenele <lucas.denele@isen-lille.fr>
2018-09-27 16:33:39 +0200

    b
    b

--- FileTree.py
+++ FileTree.py
@@ -57,28 +57,3 @@ def get_diff(tree1: [], tree2: []):
         if file not in tree2:
             file_diff.append(file)
     return file_diff
-
-
-
-# ----------------------------
-# Part 3 - main entry
-# ----------------------------
-
-if __name__ == "__main__":
-    """
-    """
-    open('tree.txt', 'w')
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
-    logger.addHandler(stream_handler)
-    file_handler = RotatingFileHandler('activity.log', 'w', 1000000, 1)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-    for i in range(0, 10):
-        run_tree_check(logger, 2, os.getcwd())
-        time.sleep(15)
